9bd581cca3c15b6fc9d623541a719b32dc020722
Camilly30 <millly.bacarim@gmail.com>
2023-08-11 17:14:03 -0400

    commit1
    atividade antiga e nova

--- servidor/index.js
+++ servidor/index.js
@@ -19,13 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
-app.use(cookieParser());
+app.use(cookieParser());//
 app.use(
   expressJWT({
     secret: process.env.SECRET,
-    algorithms: ["HS256"],
+    algorithms: ["HS256"],//usa pra fazer criptografia
     getToken: req => req.cookies.token
-  }).unless({ path: ["/autenticar", "/logar", "/deslogar", "/"] })
+  }).unless({ path: ["/autenticar", "/logar", "/deslogar"] })
 );
 
 app.get('/autenticar', async function(req, res){
@@ -37,14 +37,29 @@ app.get('/', async function(req, res){
 })
 
 app.post('/logar', (req, res) => {
-  res.send("tururururu")
-  
+  if (req.body.usuario == "Ahsoka" && req.body.senha == "123456"){
+    let id ="1";
+
+    const token = jwt.sign({id }, process.env.SECRET,{ 
+      expiresIn:3003
+    })
+    res.cookie('token',token, {httpOnly:true});
+     return res.json({
+      usuario: req.body.usuario,
+      token: token
+     })
+  }
+ res.status(500).json({mensagem :"Deu ruim aí brow"})
 })
 
-app.post('/deslogar', function(req, res) {
-  
+
+app.post('/deslogar', function(req, res) { //quando é para deslogar deleta o TOKEN
+  res.cookie('token', null, {httpOnly:true});
+   res.json({
+   deslogado:true
+   })
 })
 
 app.listen(3000, function() {
-  console.log('App de Exemplo escutando na porta 3000!')
+  console.log('App funcionando na porta 3000!')
 });
